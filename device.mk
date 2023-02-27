1d255389f346dbf005ee2b89c2a4eef99874e2fe
ralph950412 <ralph950412@gmail.com>
2023-02-28 01:00:59 +0800

    venus: Enable Android's native blur impl
    The Adreno 660 should be more than capable of handling this even though we know that there is a slight performance hit. However, this shouldn't matter all that much as blur isn't really used in a lot of places.
    
    Change-Id: I129f10a0c2c6991cd7afbc68fb6b238741c3fe44

--- device.mk
+++ device.mk
@@ -17,6 +17,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_lahaina/sound_trigger_mixer_paths.xml \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_lahaina/sound_trigger_platform_info.xml
 
+# Blur
+TARGET_USES_BLUR := true
+
 # HotwordEnrollement app permissions
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
