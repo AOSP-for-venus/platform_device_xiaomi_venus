210b636849f6a0e9c09ef7343b7c90a2e2ddd884
Michael Bestas <mkbestas@lineageos.org>
2024-08-27 22:04:35 +0300

    venus: Sync extract scripts with templates
    Change-Id: I98a36854c5a0c0390592911bb2558b34eff77efc

--- extract-files.sh
+++ extract-files.sh
@@ -1,25 +1,38 @@
 #!/bin/bash
 #
-# Copyright (C) 2020 The LineageOS Project
-#
+# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 #
 
 function blob_fixup() {
     case "${1}" in
         vendor/etc/camera/pureShot_parameter.xml)
+            [ "$2" = "" ] && return 0
             sed -i 's/=\([0-9]\+\)>/="\1">/g' "${2}"
             ;;
         vendor/lib64/hw/camera.xiaomi.so)
+            [ "$2" = "" ] && return 0
 	    "${SIGSCAN}" -p "AA 06 00 94" -P "1F 20 03 D5" -f "${2}"
             ;;
 	vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so)
+            [ "$2" = "" ] && return 0
 	    "${SIGSCAN}" -p "8D 0A 00 94" -P "1F 20 03 D5" -f "${2}"
 	    ;;
         vendor/lib64/vendor.xiaomi.hardware.cameraperf@1.0-impl.so)
+            [ "$2" = "" ] && return 0
 	    "${SIGSCAN}" -p "7C 00 00 94" -P "1F 20 03 D5" -f "${2}"
             ;;
+        *)
+            return 1
+            ;;
     esac
+
+    return 0
+}
+
+function blob_fixup_dry() {
+    blob_fixup "$1" ""
 }
 
 # If we're being sourced by the common script that we called,
@@ -30,9 +43,9 @@ fi
 
 set -e
 
-# Required!
 export DEVICE=venus
 export DEVICE_COMMON=sm8350-common
 export VENDOR=xiaomi
+export VENDOR_COMMON=${VENDOR}
 
-"./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
+"./../../${VENDOR_COMMON}/${DEVICE_COMMON}/extract-files.sh" "$@"
