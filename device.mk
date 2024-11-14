0e776dbd711b17755cc75a0c4370c7636a10c05c
Arian <arian.kulmer@web.de>
2024-11-14 21:45:29 +0800

    venus: Add v2 suffix to sensors.xiaomi
    Change-Id: Ie6ef0e5fd4653eae298a3455edf14e05508e1cfd

--- device.mk
+++ device.mk
@@ -50,7 +50,7 @@ $(call inherit-product, vendor/hardware/xiaomi/aidl/powershare/product.mk)
 
 # Sensors
 PRODUCT_PACKAGES += \
-    sensors.xiaomi
+    sensors.xiaomi.v2
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
